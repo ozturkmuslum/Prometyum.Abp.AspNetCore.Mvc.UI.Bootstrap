5b3c8b9d9a08575504ce2ef8398c2b1f284fdd8d
mozturk <muslum.ozturk@yaz.com.tr>
2020-11-01 13:15:52 +0300

    selected all text when autocomplete taghelper on focused

--- src/Prometyum.Abp.AspNetCore.Mvc.UI.Bootstrap/wwwroot/js/taghelpers.js
+++ src/Prometyum.Abp.AspNetCore.Mvc.UI.Bootstrap/wwwroot/js/taghelpers.js
@@ -1,54 +1,49 @@
-﻿ $('.pro-autocomplete').each(function(index, el) {
-            var $el = $(el);
+﻿$('.pro-autocomplete').each(function (index, el) {
+    var $el = $(el);
 
-            var defaultValue = $el.data("default-value");
-            var onSelectedChanged = $el.data("on-selected-changed");
+    var defaultValue = $el.data("default-value");
+    var onSelectedChanged = $el.data("on-selected-changed");
 
-            $el.autoComplete({
-                resolverSettings: {
-                    url: $el.data("source"),
-                    queryKey: 'q'
-                },
-                minLength: parseInt($el.data("min-length")),
-                //defaultValue:3,
-                //defaultText:"Docker",
-                //placeholder:"type to search..." 
-                noResultsText: $el.data("no-results-text")
-            });
+    $el.autoComplete({
+        resolverSettings: {
+            url: $el.data("source"),
+            queryKey: 'q'
+        },
+        minLength: parseInt($el.data("min-length")),
+        //defaultValue:3,
+        //defaultText:"Docker",
+        //placeholder:"type to search..." 
+        noResultsText: $el.data("no-results-text")
+    });
 
-            $el.on('change', function(e,item) {
-                // console.log('change');
-                autoCompleteValueChanged($el, onSelectedChanged,item.value, item.text);
-            });
-            
-            $el.on('autocomplete.select',
-                function(evt, item) {
-                    autoCompleteValueChanged($el, onSelectedChanged,item.value, item.text);
-                });
+    $el.on('change', function (e, item) {
+        // console.log('change');
+        autoCompleteValueChanged($el, onSelectedChanged, item.value, item.text);
+    });
 
-            $el.on('autocomplete.freevalue',
-                function(evt, value) {
-                   
-                    autoCompleteValueChanged($el, onSelectedChanged,defaultValue, value);
-                });
-
-            var focusedElement;
-
-            $el.on('focus', function () {
-                if (focusedElement === this) return; //already focused, return so user can now place cursor at specific point in input.
-                focusedElement = this;
-                setTimeout(function () { focusedElement.select(); }, 50); //select all text in any field on focus for easy re-entry. Delay sightly to allow focus to "stick" before selecting.
-            });
+    $el.on('autocomplete.select',
+        function (evt, item) {
+            autoCompleteValueChanged($el, onSelectedChanged, item.value, item.text);
         });
 
-        function autoCompleteValueChanged($el,onSelectedChanged,value,text) {
-                
-            $($el.data("hidden-unique-class")).val(value);
+    $el.on('autocomplete.freevalue',
+        function (evt, value) {
+            autoCompleteValueChanged($el, onSelectedChanged, defaultValue, value);
+        });
 
-            if (onSelectedChanged) {
-                setTimeout(onSelectedChanged + "('" + value + "','"+text+"')", 5);
-            };
-        }
+    $el.on('focus', function (e) {
+        setTimeout(function () { e.currentTarget.select(); }, 50); //select all text in any field on focus for easy re-entry. Delay sightly to allow focus to "stick" before selecting.
+    });
+});
+
+function autoCompleteValueChanged($el, onSelectedChanged, value, text) {
+
+    $($el.data("hidden-unique-class")).val(value);
+
+    if (onSelectedChanged) {
+        setTimeout(onSelectedChanged + "('" + value + "','" + text + "')", 5);
+    };
+}
 
 //// user then clicks on some button and we need to change that default value
 //$('.btnChangeAutoSelect').on('click',
